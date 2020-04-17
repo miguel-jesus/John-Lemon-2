921a56216a31fb6595c8f311e07849bf77bb82de
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-04-17 13:13:14 +0200

    first commit

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource caughtAudio;
+    public float displayImageDuration = 1f;
+
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+    bool m_HasAudioPlayed;
+    float m_Timer;
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        //en cuanto el pj entre al trigger y sea igual al player,nuestro booleano pasara a ser true
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup,true, caughtAudio);
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        //los sonidos solo se toquen una vez
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+
+        }
+        //cronómetro que empieza a contar desde el momento que lo cruza
+        m_Timer += Time.deltaTime;
+        //Duración del desvanecimiento y asi iremos incrementando el alpga cada vez mas porque llamaremos a esta funcion
+        //desde el update
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+    
+}
